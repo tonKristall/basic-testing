b31912457c706a084a74800ded027345835bc84a
Aleksander Shustov <krisale@yandex.ru>
2024-10-16 21:56:12 +0300

    feat: table tests

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,17 +1,30 @@
-// Uncomment the code below and write your tests
-/* import {  simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 const testCases = [
-    { a: 1, b: 2, action: Action.Add, expected: 3 },
-    { a: 2, b: 2, action: Action.Add, expected: 4 },
-    { a: 3, b: 2, action: Action.Add, expected: 5 },
-    // continue cases for other actions    
-]; */
+  { a: 1, b: 2, action: Action.Add, expected: 3 },
+  { a: 2, b: 2, action: Action.Add, expected: 4 },
+  { a: 3, b: 2, action: Action.Add, expected: 5 },
+  { a: 3, b: 2, action: Action.Subtract, expected: 1 },
+  { a: 4, b: 3, action: Action.Subtract, expected: 1 },
+  { a: 5, b: 1, action: Action.Subtract, expected: 4 },
+  { a: 3, b: 2, action: Action.Multiply, expected: 6 },
+  { a: 5, b: 1, action: Action.Multiply, expected: 5 },
+  { a: 4, b: 2, action: Action.Multiply, expected: 8 },
+  { a: 6, b: 2, action: Action.Divide, expected: 3 },
+  { a: 5, b: 1, action: Action.Divide, expected: 5 },
+  { a: 4, b: 2, action: Action.Divide, expected: 2 },
+  { a: 3, b: 2, action: Action.Exponentiate, expected: 9 },
+  { a: 2, b: 2, action: Action.Exponentiate, expected: 4 },
+  { a: 5, b: 1, action: Action.Exponentiate, expected: 5 },
+  { a: 5, b: 1, action: 'invalid', expected: null },
+  { a: 'a', b: 'b', action: Action.Add, expected: null },
+];
 
 describe('simpleCalculator', () => {
-  // This test case is just to run this test suite, remove it when you write your own tests
-  test('should blah-blah', () => {
-    expect(true).toBe(true);
-  });
-  // Consider to use Jest table tests API to test all cases above
+  it.each(testCases)(
+    'should calculate $a $action $b = $expected',
+    ({ a, b, action, expected }) => {
+      expect(simpleCalculator({ a, b, action })).toBe(expected);
+    },
+  );
 });
